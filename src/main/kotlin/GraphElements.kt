e47594576629e69c7779d9cfc41458255e09f2b1
Bojan Ludajic <bojan.ludajic@gmail.com>
2025-09-25 00:20:20 +0200

    move node interface together with stmt and expr

--- src/main/kotlin/GraphElements.kt
+++ src/main/kotlin/GraphElements.kt
@@ -23,3 +23,18 @@ sealed interface Expr {
     class Mul(val left: Expr, val right: Expr) : Expr
 }
 
+sealed interface Node {
+    class Assign(
+        val variable: Expr.Var,
+        val value: Expr,
+        val next: Node
+    ): Node
+    class Return(val result: Expr): Node
+    class Condition(
+        val cond: Expr,
+        val nextIfTrue: Node,
+        val nextIfFalse: Node
+    ): Node
+    object Quit : Node
+}
+
